b5399fb5c2aa3c559a6dd57c967245ef1e0394c7
Virang Kathiriya <virangkathiriya1@gmail.com>
2026-02-07 23:08:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,1 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// basic vite config for react app
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  }
-})
